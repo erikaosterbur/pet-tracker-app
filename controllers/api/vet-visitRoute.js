db6132ee9fb1f4b77f13a183af6caad1d3ae379a
Jack Carpenter <jackcarpenter1@live.com>
2021-11-01 12:24:18 -0500

    Adding vet visit routing

---
+++ controllers/api/vet-visitRoute.js
@@ -0,0 +1,94 @@
+const router = require('express').Router();
+const { Vet, Pet } = require('../../models');
+
+//Get all visit
+//Get visit by id
+//Create visit
+//Update visit
+//Delete visit
+
+
+//Pulling all vet visits
+router.get('/', async (req, res) => {
+    try {
+      const vetData = await Vet.findAll({
+        include: [{model: Pet}]
+      });
+      res.status(200).json(vetData);
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+});
+
+//Pulling vet visit by id
+router.get('/:id', async (req, res) => {
+    try {
+      const vetData = await Vet.findByPk(req.params.id, {
+        indclude: [{ model: Pet }]
+      });
+  
+      if (!vetData) {
+        res.status(404).json({ message: "No vet visit found with that id :("})
+        return;
+      }
+  
+      res.status(200).json(vetData);
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err);
+    }
+});
+
+//Create new vet visit
+router.post('/', async (req, res) => {
+    try {
+      const vetData = await Vet.create(req.body);
+      res.status(200).json(vetData);
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err)
+    }
+
+});
+
+//Update Vet visit
+//Likely needs tweaking
+router.put('/:id', (req, res) => {
+    Vet.update( req.body, {
+//This part is iffy
+      where: {
+        date: req.params.id,
+        description: req.params.id,
+        weight: req.params.id,
+      }
+    })
+    .then((updatedVet) => {
+      res.json(updatedVet)
+    })
+    .catch((err) => res.json(err),
+    console.log(err))
+});
+
+//Delete a vet visit
+router.delete('/:id', async (req, res) => {
+    try {
+      const vetData = await Vet.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if(!vetData) {
+        res.status(404).json({ message: "No vet visit found with that id :("})
+        return;
+      }
+  
+      res.status(200).json(vetData);
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err);
+    }
+});
+
+module.exports = router;
