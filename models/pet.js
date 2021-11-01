f041175a73ab45572201fc647a8737067e267479
Jack Carpenter <jackcarpenter1@live.com>
2021-11-01 10:46:21 -0500

    Pushing up models and relationships

---
+++ models/pet.js
@@ -0,0 +1,61 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Pet extends Model {}
+
+// set up fields and rules for Pet model
+Pet.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pet_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dateofbirth: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+        unique: false
+      }
+    },
+    vet_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'vet',
+            key: 'id',
+            unique: false,
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'pet',
+  }
+);
+
+module.exports = Pet;
