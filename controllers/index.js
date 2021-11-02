f7f2f3f765f12ba487f1ffced937f1e15dfa1d46
erikaosterbur <erikaosterbur@gmail.com>
2021-11-02 13:52:51 -0500

    render page bug fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoute');
+const dashboardRoutes = require('./dashboardRoute');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
