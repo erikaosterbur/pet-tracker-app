b41bff0966c40f9bba0ca482baebbbe51350c154
erikaosterbur <erikaosterbur@gmail.com>
2021-11-03 11:29:22 -0500

    delete pet, update pet buttons working

--- public/js/edit-pet.js
+++ public/js/edit-pet.js
@@ -7,43 +7,25 @@ const delButtonHandler = async (event) => {
     });
 
         if (response.ok) {
+            window.alert('Sorry for your loss!');
             document.location.replace('/dashboard');
         } else {
             alert('Failed to delete pet');
         }
 };
 
-const updateButtonHandler = async (event) => {
-    event.preventDefault();
-
-    const pet_id = document.querySelector('#edit-pet-id').value;
-    const pet_name = document.querySelector('#edited-name').value.trim();
-    const pet_type = document.querySelector('#edited-type').value.trim();
-    const pet_color = document.querySelector('#edited-color').value.trim();
-    const pet_dob = document.querySelector('#edited-DOB').value.trim();
-
-
-    if (pet_name && pet_type && pet_color && pet_dob) {
-        const response = await fetch(`/api/pets/${pet_id}`, {
-            method: 'PUT',
-            body: JSON.stringify({pet_name, pet_type, pet_color, pet_dob}),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Failed to update pet');
-        }
-    }
-}
+// const updateButtonHandler = async (event) => {
+//     event.preventDefault();
+//     res.render('edit-pet', { 
+//         layout: 'dashboard',
+//         logged_in: req.session.logged_in,
+//       });
+// }
 
 document
     .querySelector('#delete-pet-button')
     .addEventListener('click', delButtonHandler);
 
-document
-    .querySelector('#edit-pet')
-    .addEventListener('click', updateButtonHandler);
+// document
+//     .querySelector('#update-pet-button')
+//     .addEventListener('click', updateButtonHandler);
